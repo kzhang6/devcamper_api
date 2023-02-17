aa4981d569694a6ce0ec249913942ecc8189414a
Kristine Zhang <kristine.zhang@cognosante.com>
2023-02-16 20:10:45 -0800

    Send a successful response

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config({ path: "./config/config.env" });
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(400).json({ success: false });
+  res.status(200).json({ success: true, data: { id: 1 } });
 });
 
 const PORT = process.env.PORT || 5000;
