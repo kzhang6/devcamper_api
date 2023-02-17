6436f301841b5f2c040832b977a435714ab45fe9
Kristine Zhang <kristine.zhang@cognosante.com>
2023-02-16 20:06:04 -0800

    We can send some html since the Content-Type is text/html

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello from express</h1>");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
